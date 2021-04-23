697481e147324cf68504cc7ff3a4253c21ad4843
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-04-23 12:37:41 -0700

    Completed stretch task

--- index.js
+++ index.js
@@ -242,9 +242,24 @@ console.log(cuboidTwo.surfaceArea()); // 130
 
 // 🦄 💪 Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area. 🦄 💪
   
+class CubeMaker extends CuboidMaker{
+    constructor(param){
+        super(param);
+    }
+    volume(){
+        return Math.pow(this.length, 3);
+    }
+    surfaceArea(){
+        return 6 * Math.pow(this.length, 2);
+    }
+}
 
+const cube = new CubeMaker({
+    length: 5
+});
 
-
+console.log(cube.volume());
+console.log(cube.surfaceArea());
 
 
   /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
